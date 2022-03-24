0cb3a73ddbe16f8535f0707da7b12c3399c911c4
ido-bakimer <102211590+ido-bakimer@users.noreply.github.com>
2022-03-24 20:02:06 +0200

    Delete js directory

--- js/7boom.js
+++
@@ -1,7 +0,0 @@
-function sevenBoom(elBoomBtn) {
-    if (gGame.secsPassed) return;
-    if (gGame.isManual) return;
-    if (gGame.isSeven) return;
-    elBoomBtn.innerText = '7BOOM is on!'
-    gGame.sevenMode = true
-}
